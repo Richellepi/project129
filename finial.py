4bafd877f11bf11788cd48fcb31a60d627e6f601
Richellepi <96777032+Richellepi@users.noreply.github.com>
2022-01-13 20:53:08 +0530

    Add files via upload

---
+++ finial.py
@@ -0,0 +1,39 @@
+import csv
+from email import header
+
+data1 = []
+data2=[]
+
+
+with open("dataset_1.csv", "r") as f:
+    csvreader = csv.reader(f)
+    for row in csvreader: 
+        data1.append(row)
+
+
+with open("dataset_2_sorted.csv", "r") as f:
+    csvreader = csv.reader(f)
+    for row in csvreader: 
+        data2.append(row)
+
+
+headers1 = data1[0]
+planet_data1 = data1[1:]
+
+headers2=data2[0]
+planet_data2=data2[1:]
+
+headers=headers1+headers2
+
+planet_data=[]
+for index,data in enumerate(planet_data1):
+    planet_data.append(planet_data1[index]+ planet_data2[index])
+    
+
+
+with open("finial.csv", "a+") as f:
+    csvwriter = csv.writer(f)
+    csvwriter.writerow(headers)
+    csvwriter.writerows(planet_data)
+
+
